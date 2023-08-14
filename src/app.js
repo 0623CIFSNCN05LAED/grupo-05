485c838a847ca10a4622a1a93a568c726cdfd4dd
magali-anglada <magali.anglada@gmail.com>
2023-08-13 21:35:24 -0300

    Devvision: Webpage initial structure

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const PORT = 3002;
+
+// 1) Importar tanto el framework Express y path.
+const express = require('express');
+const path = require('path');
+
+// 2) Crear una instancia de Express.
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+// 3) Poner al servidor a escuchar al puerto 3001.
+app.listen(PORT, () => console.log('Se prendió en el puerto ' + PORT));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+})
