e256cc794bd4019b9ebd0767e89c5dbdb9cab0b9
magali-anglada <magali.anglada@gmail.com>
2023-08-15 21:47:09 -0300

    Initial structure for the e-commerce Home page and Styles sheet definition

--- src/app.js
+++ src/app.js
@@ -15,3 +15,20 @@ app.listen(PORT, () => console.log('Se prendió en el puerto ' + PORT));
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'));
 })
+
+app.get('/index', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+})
+app.get('/register', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+})
+app.get('/login', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+})
+app.get('/productCart', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/productCart.html'));
+})
+app.get('/productDetail', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/productDetail.html'));
+})
+
