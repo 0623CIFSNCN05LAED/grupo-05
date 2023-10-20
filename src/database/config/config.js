1bd9565e3e0b4949df3b2d77120779fce34b0590
magali-anglada <magali.anglada@gmail.com>
2023-10-20 08:40:36 -0300

    Sequelizer installation. Database and Models folders were added

---
+++ src/database/config/config.js
@@ -0,0 +1,24 @@
+
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "devvision_dev",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "devvision_qas",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "devvision_prd",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }   
+}
---
+++ src/database/models/sizes.js
@@ -0,0 +1,17 @@
+/*
+module.exports = (sequelize, DataTypes) => {
+
+    const Model = sequelize.define('Sizes', 
+    {
+        id: DataTypes.INTEGER, 
+        name: DataTypes.STRING,
+        shortName: DataTypes.STRING,     
+    }, 
+    {
+        tableName: 'sizes',
+            
+    });     
+ 
+    return Model;
+};
+*/
