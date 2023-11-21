d3ac29ecc7e13838d7a30a18a507ae393a8530b3
magali-anglada <magali.anglada@gmail.com>
2023-11-20 21:41:39 -0300

    Iniciao de implementación de validaciones con Validator en Product New

---
+++ public/js/product-new-validator.js
@@ -0,0 +1,68 @@
+const validations = [
+    {
+      field: "art",
+      check: (input) => input.value.length >= 3,
+      message: "Debe contener al menos tres caracteres",
+    }
+ 
+/*     {
+      field: "release_date",
+      check: (input) => validator.isDate(input.value),
+      message: "Debe ser una fecha válida",
+    }, 
+*/
+
+  ];
+  
+  validations.forEach((validation) => {
+    const inputId = validation.field;
+    const input = document.getElementById(inputId);
+    const inputErrorMsg = document.getElementById(inputId + "Error");
+  
+    function validate() {
+      console.log("input.value", input.value);
+      inputValidation(validation, input, inputErrorMsg);
+    }
+  
+    input.addEventListener("blur", validate);
+    input.addEventListener("input", validate);
+  });
+  
+  const form = document.getElementById("form");
+  
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+  
+    const validationsResult = [];
+  
+    validations.forEach((validation) => {
+      const inputId = validation.field;
+      const input = document.getElementById(inputId);
+      const inputErrorMsg = document.getElementById(inputId + "Error");
+      const result = inputValidation(validation, input, inputErrorMsg);
+      validationsResult.push(result);
+    });
+  
+    if (validationsResult.every((val) => val == true)) {
+      form.submit();
+    }
+  });
+  
+  function inputValidation(validation, input, inputErrorMsg) {
+    if (!input.value) {
+      inputErrorMsg.innerText = "El campo no debe estar vacío";
+      inputErrorMsg.classList.add("display");
+      return false;
+    }
+  
+    if (!validation.check(input)) {
+      inputErrorMsg.innerText = validation.message;
+      inputErrorMsg.classList.add("display");
+      return false;
+    }
+  
+    inputErrorMsg.innerText = "";
+    inputErrorMsg.classList.remove("display");
+    return true;
+  }
+  
