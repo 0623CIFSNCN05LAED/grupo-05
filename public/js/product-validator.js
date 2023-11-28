cb075a72acaf740eac397864c09716aabf31ed65
magali-anglada <magali.anglada@gmail.com>
2023-11-27 23:16:33 -0300

    Validaciones back y front en productos

--- public/js/product-validator.js
+++ public/js/product-validator.js
@@ -1,24 +1,49 @@
 const validations = [
     {
       field: "art",
-      check: (input) => true,
-      message: "",
+      check: (input) => input.value.length <= 100,
+      message: "Debe contener como màximo 100 caracteres",
     },
     {
       field: "name",
-      check: (input) => input.value.length >= 5,
-      message: "Debe contener al menos 3 caracteres",
+      check: (input) => ( input.value.length >= 5 && input.value.length <= 100 ),
+      message: "Debe contener al menos 5 caracteres y como máximo 100",
     },
     {
       field: "description",
-      check: (input) => input.value.length >= 20,
-      message: "Debe contener al menos 20 caracteres",
+      check: (input) => ( input.value.length >= 20 && input.value.length <= 500 ),
+      message: "Debe contener al menos 20 caracteres y como máximo 500",
+    },
+    {
+      field: "brand",
+      check: (input) => input.value.length > 0,
+      message: "Debe especificar una marca",
+    },
+    {
+      field: "collection",
+      check: (input) => input.value.length <= 100,
+      message: "Debe contener como máximo 100 caracteres",
+    },
+    {
+      field: "model",
+      check: (input) => input.value.length <= 100,
+      message: "Debe contener como máximo 100 caracteres",
+    },    
+    {
+      field: "gender",
+      check: (input) => input.value.length > 0,
+      message: "Debe especificar un genero",
+    },
+    {
+      field: "year",
+      check: (input) => ( input.value.length >= 1900 && input.value.length <= 2099 ),
+      message: "Debe ser un año entre 1900 y 2099",
+    },
+    {
+      field: "price",
+      check: (input) => input.value.length > 0,
+      message: "",
     }
-/*  {
-      field: "image",
-      check: (input) => input.value.length >= 20,
-      message: "Debe contener al menos 20 caracteres",
-    },*/
   ];
   
   validations.forEach((validation) => {
--- src/validations/validation-product.js
+++ src/validations/validation-product.js
@@ -1,17 +1,40 @@
 const { body } = require("express-validator");
 
-
 module.exports = {
   product_new: [
+    body("art").notEmpty().withMessage("Debe completar este campo")
+    .isLength({ min: 1, max: 100 }).withMessage("Debe contener como máximo 100 caracteres"),
     body("name").notEmpty().withMessage("Debe completar este campo")
-      .isLength({ min: 5, max: 100 }).withMessage("Debe tener al menos 5 caracteres"),
-    body("description").isLength({ min: 20, max: 500 }).withMessage("Debe tener al menos 20 caracteres"),
+    .isLength({ min: 5, max: 100 }).withMessage("Debe contener al menos 5 caracteres y como máximo 100"),
+    body("description").notEmpty().withMessage("Debe completar este campo")
+    .isLength({ min: 20, max: 500 }).withMessage("Debe contener al menos 20 caracteres y como máximo 500"),
+    body("brand").notEmpty().withMessage("Debe especificar una marca"),
+    body("collection").notEmpty().withMessage("Debe completar este campo")
+    .isLength({ min: 1, max: 100 }).withMessage("Debe contener como máximo 100 caracteres"),
+    body("model").notEmpty().withMessage("Debe completar este campo")
+    .isLength({ min: 1, max: 100 }).withMessage("Debe contener como máximo 100 caracteres"),
+    body("gender").notEmpty().withMessage("Debe especificar un gener"),
+    body("year").notEmpty().withMessage("Debe especificar un gener")
+    .isInt({ min: 1900, max: 2099 }).withMessage("Debe ser un año entre 1900 y 2099"),
+    body("price").notEmpty().withMessage("Debe especificar un gener"),
     body("image").isIn(["JPG", "PNG", "JPEG", "GIF"]).withMessage("Debe ingresar un imagien (JPG/PNG/JPEG/GIF)"),
   ],
   product_edit: [
+    body("art").notEmpty().withMessage("Debe completar este campo")
+    .isLength({ min: 1, max: 100 }).withMessage("Debe contener como máximo 100 caracteres"),
     body("name").notEmpty().withMessage("Debe completar este campo")
-      .isLength({ min: 5, max: 100 }).withMessage("Debe tener al menos 5 caracteres"),
-    body("description").isLength({ min: 20, max: 500 }).withMessage("Debe tener al menos 20 caracteres"),
-    body("image").isIn(["JPG", "PNG", "JPEG", "GIF"]).withMessage("Debe ingresar un imagien (JPG/PNG/JPEG/GIF"),
+    .isLength({ min: 5, max: 100 }).withMessage("Debe contener al menos 5 caracteres y como máximo 100"),
+    body("description").notEmpty().withMessage("Debe completar este campo")
+    .isLength({ min: 20, max: 500 }).withMessage("Debe contener al menos 20 caracteres y como máximo 500"),
+    body("brand").notEmpty().withMessage("Debe especificar una marca"),
+    body("collection").notEmpty().withMessage("Debe completar este campo")
+    .isLength({ min: 1, max: 100 }).withMessage("Debe contener como máximo 100 caracteres"),
+    body("model").notEmpty().withMessage("Debe completar este campo")
+    .isLength({ min: 1, max: 100 }).withMessage("Debe contener como máximo 100 caracteres"),
+    body("gender").notEmpty().withMessage("Debe especificar un gener"),
+    body("year").notEmpty().withMessage("Debe especificar un gener")
+    .isInt({ min: 1900, max: 2099 }).withMessage("Debe ser un año entre 1900 y 2099"),
+    body("price").notEmpty().withMessage("Debe especificar un gener"),
+    body("image").isIn(["JPG", "PNG", "JPEG", "GIF"]).withMessage("Debe ingresar un imagien (JPG/PNG/JPEG/GIF)"),
   ]
-}
+};
