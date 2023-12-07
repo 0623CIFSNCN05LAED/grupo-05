faaf81f41431c4fd0fc75f7ac031f790763e42af
magali-anglada <magali.anglada@gmail.com>
2023-12-06 21:55:37 -0300

    Aggregado de URLs en apis de consultas de producto

--- src/controllers/api/products-controller.js
+++ src/controllers/api/products-controller.js
@@ -1,5 +1,5 @@
 const productService = require("../../services/product-services");
-
+const path = require("path");
 const pageSize = 10
 
 module.exports = {
@@ -28,6 +28,17 @@ module.exports = {
     const products = await productService.getAllProductsOverview();
     const totalProductsByCategory = await productService.getTotalProductsByCategory();
 
+    const apiProducts = products.map((product) =>{
+      return {
+        id: product.id,
+        name: product.name,
+        description: product.description,
+        colors: product.colors,
+        sizes: product.sizes,
+        detail: req.headers.host + req.originalUrl + "/" + product.id,
+      };
+    });
+
     res.json({
         meta: {
             status: 200,
@@ -35,17 +46,24 @@ module.exports = {
             totalsByCategory: totalProductsByCategory,
             url: req.originalUrl
         }, 
-        data: products 
+        data: apiProducts 
     })
   },  
   detail: async(req, res) => {
     const product = await productService.getProduct(req.params.id);
+    const pathImage = path.resolve(__dirname,"../../../public/images/products");
+    const urlImage = pathImage + "\\" + product.image;
+    
+    const apiProduct = {
+      product: product,
+      urlImage: urlImage
+    };
 
     res.json({
       meta: {
         status: 200,
-        product: product,
-        url: '/api/products/' + req.params.id,
+        data: apiProduct,
+        url: req.originalUrl
       },
     });
   }
