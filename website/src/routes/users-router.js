2d644e52097f5d5c8499113d4d32f9dd20fe44b8
afarbelaezv <afarbelaez10@gmail.com>
2024-01-02 15:06:43 -0500

    Correccion guardado de imagenes de usuarios

--- website/src/routes/users-router.js
+++ website/src/routes/users-router.js
@@ -2,16 +2,15 @@ const { Router, urlencoded } = require("express");
 const router = Router();
 const validationsAuth = require("../validations/validation-auth");
 const validateforms = require("../middlewares/validate-form");
-const userGuard = require("../middlewares/user-guard")
+const userGuard = require("../middlewares/user-guard");
 const adminGuard = require("../middlewares/admin-guard");
-const userInfoGuard  =require("../middlewares/users-info-guard")
+const userInfoGuard = require("../middlewares/users-info-guard");
 const multer = require("multer");
 
 // ** Controller Requires's -----------------------------------------------------------------------------------
 const usersController = require("../controllers/users-controller");
 // Multer Users
-const upload = require("../middlewares/multer-product");
-
+const upload = require("../middlewares/multer-users");
 
 // Main Routes
 router.get("/register", usersController.showRegister);
@@ -35,19 +34,20 @@ router.post(
   usersController.register
 );
 
-
 // Listado de usuarios
-router.get("/list",userGuard,adminGuard, usersController.userList);
+router.get("/list", userGuard, adminGuard, usersController.userList);
 
 //Buscar usuario
-router.get("/search",userGuard,adminGuard, usersController.search);
+router.get("/search", userGuard, adminGuard, usersController.search);
 
 // Detalle de un usuario particular
-router.get("/detail/:id",userGuard,userInfoGuard, usersController.userDetail);
-router.get("/edit/:id",userGuard,userInfoGuard, usersController.userEdit);
+router.get("/detail/:id", userGuard, userInfoGuard, usersController.userDetail);
+router.get("/edit/:id", userGuard, userInfoGuard, usersController.userEdit);
 //Acción de edición
 router.post(
-  "/update/:id",userGuard,userInfoGuard,
+  "/update/:id",
+  userGuard,
+  userInfoGuard,
   upload.single("image"),
   urlencoded({ extended: false }),
   usersController.update
@@ -55,12 +55,14 @@ router.post(
 
 // Acción de actualización depermisos desde el admin
 router.post(
-  "/updatebyadmin/:id",userGuard,adminGuard,
+  "/updatebyadmin/:id",
+  userGuard,
+  adminGuard,
   urlencoded({ extended: false }),
   usersController.updateByAdmin
 );
 
 //Acción de borrado
-router.get("/delete/:id",userGuard,adminGuard, usersController.delete);
+router.get("/delete/:id", userGuard, adminGuard, usersController.delete);
 
 module.exports = router;
